7dd5b4e4c649830320cf38452ae9c8dfdcc1bed5
dougdboliveira <douglasdboliveira@gmail.com>
2021-04-08 18:37:17 -0300

    first integration with pokeapi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from './PokemonList';
+import axios from 'axios';
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/').then(res => {
+      setPokemon(res.data.results.map(pokemon => pokemon.name));
+    });
+  }, []);
 
   return (
     <PokemonList pokemon={pokemon} />
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PokemonList({ pokemon }) {
     return (
         <div>
-            {pokemon.map((p) => (<div key={p}>{p}</div>) )}
+            <div>{pokemon.map(pokemon => (<div key={pokemon}>{pokemon}</div>))}</div>
         </div>
     )
 }
