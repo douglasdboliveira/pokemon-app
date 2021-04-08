b65b1f917df10192d5d92beef2655fc1e7400262
dougdboliveira <douglasdboliveira@gmail.com>
2021-04-08 12:05:16 -0300

    rendering some data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PokemonList from './PokemonList';
 
 function App() {
+  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
+
   return (
-    null
+    <PokemonList pokemon={pokemon} />
   );
 }
 
